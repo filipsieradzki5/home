c0c23016013804fa9df7d68c1193c24c942c9d4b
Filip Sieradzki <sieradzki.filiP@gmail.comp>
2023-02-01 23:32:57 +0100

    added about section

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const nav = document.querySelector('nav');
 const homeNav = document.querySelector('.home_');
 const workNav = document.querySelector('.work_');
 const contactNav = document.querySelector('.contact_');
+const aboutNav = document.querySelector('.about_');
 const image =document.querySelectorAll('.img_split');
 
 const wait = (amount = 0) => new Promise(resolve => setTimeout(resolve, amount));
@@ -13,15 +14,19 @@ let observer = new IntersectionObserver(navCheck, {
 
 function navCheck(entries) {
     entries.forEach(entry => {
+        
         if (entry.isIntersecting && entry.target.nodeName == 'MAIN') {
             homeNav.classList.add('red');
         } else if (entry.isIntersecting && entry.target.nodeName == 'DIV') {
             workNav.classList.add('red');
             image.forEach(image => image.classList.add('appear'));
+            
 
         } else if (entry.isIntersecting && entry.target.nodeName == 'FOOTER') {
             contactNav.classList.add('red')
-
+            
+        } else if (entry.isIntersecting && entry.target.nodeName == 'MAIN') {
+            aboutNav.classList.add('red')
         } else {
             homeNav.classList.remove('red');
             workNav.classList.remove('red');
